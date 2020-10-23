812d61a65dd1346be7c063a9aa71b7de12c50199
BMTrotter <72217508+BMTrotter@users.noreply.github.com>
2020-10-22 21:00:09 -0400

    Assignment3
    Functions

---
+++ Assignment3/PlotVector.m
@@ -0,0 +1,12 @@
+function [PlotOfVectors] = PlotVector(Vector1,Vector2)
+%PlotVector function plots two vectors
+%   This function will plot two vectors on one plot. User must input two
+%   vectors.
+
+plot(Vector1,Vector2)
+title("2-Vector Plot")
+xlabel("x-Axis")
+ylabel("y-Axis")
+
+end
+
