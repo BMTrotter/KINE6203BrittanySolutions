ce9ac4e20a70f584113b6f1224f52659dc992088
BMTrotter <72217508+BMTrotter@users.noreply.github.com>
2020-10-22 21:08:31 -0400

    FunctionPractice

--- Assignment2/MultiplyBy2.m
+++
@@ -1,6 +0,0 @@
-function [Answer] = MultiplyBy2(Number)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-Answer = Number*2
-end
-
--- Assignment2/SumDiffProductMax.m
+++
@@ -1,9 +0,0 @@
-function [Sum,Difference,Product,Max] = SumDiffProductMax(Number1,Number2)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-Sum = Number1+Number2;
-Difference = Number1-Number2;
-Product = Number1*Number2;
-Max = max(Number1,Number2);
-end
-
